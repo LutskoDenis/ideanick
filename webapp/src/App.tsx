6916903983c8824fc2ed297508c2df6b6208cd07
LutskoDenis <dd.lutsko03@gmail.com>
2026-02-02 23:50:56 +0300

    second

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -18,12 +18,12 @@ function App() {
       </div>
       <h1>Vite123 + React123123</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
